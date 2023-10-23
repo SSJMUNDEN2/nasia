7db6ddc71be3e019a0b22ecd311235b52f19bcc8
SSJMUNDEN2 <mukunthan@premraj.org>
2023-10-23 15:54:46 +0200

    close #28

--- src/main/java/ch/zhaw/nasia/controller/KundenController.java
+++ src/main/java/ch/zhaw/nasia/controller/KundenController.java
@@ -1,5 +1,17 @@
 package ch.zhaw.nasia.controller;
 
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ch.zhaw.nasia.Kunden.Kunden;
+
+@RestController
+
 public class KundenController {
-    
+    private ArrayList<Kunden> listofKunden;
+
+    @GetMapping("/kunden")
+    public ArrayList<Kunden> getKunden() {
+        return listofKunden;
+    }
 }
