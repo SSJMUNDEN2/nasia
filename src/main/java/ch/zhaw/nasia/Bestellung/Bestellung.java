361c8336b3a7238a4ffca6288fdc78a3b5e5a17a
SSJMUNDEN2 <mukunthan@premraj.org>
2023-10-23 15:14:38 +0200

    close #3

--- src/main/java/ch/zhaw/nasia/Bestellung/Bestellung.java
+++ src/main/java/ch/zhaw/nasia/Bestellung/Bestellung.java
@@ -3,19 +3,14 @@ package ch.zhaw.nasia.Bestellung;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 
 public class Bestellung {
-    
     private int bestellungId;
     private int kundenId;
     private int lieferantId;
     private int bestellungGerichtId;
     private int lieferdauer;
     private Status status;
-
-
-
 }
