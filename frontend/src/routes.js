3aa48024b836b7d8739b50fffd82a8a2bfa10ff7
SSJMUNDEN2 <mukunthan@premraj.org>
2023-11-17 19:01:59 +0100

    .

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,19 +2,4 @@ import Home from "./pages/Home.svelte";
 import Freelancers from "./pages/Freelancers.svelte";
 import Jobs from "./pages/Jobs.svelte";
 import Bestellung from "./pages/Bestellung.svelte";
-import BestellungGericht from "./pages/BestellungGericht.svelte";
-import Gericht from "./pages/Gericht.svelte";
-import Kunden from "./pages/Kunden.svelte";
-import Lieferanten from "./pages/Lieferanten.svelte";
-
-export default {
-    '/': Home,
-    '/home': Home,
-    '/freelancers': Freelancers,
-    '/bestellung': Bestellung,
-    '/bestellungGericht': BestellungGericht,
-    '/gericht': Gericht,
-    '/kunden': Kunden,
-    '/lieferanten': Lieferanten,
-    '/jobs': Jobs
-}
+import BestellungGericht from "./pages/BestellungGericht.sv
